43f791de7c4d2bc749ff2806bfca501e96f0c8aa
maksym <maksym.varash2007@gmail.com>
2024-07-15 11:26:53 +0300

    have created MyProfile.java, SetsCards.java, Tests.java

--- app/src/main/java/com/example/educards/MainActivity.java
+++ app/src/main/java/com/example/educards/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.educards;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,9 +14,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LinearLayout start_btn;
+    private ImageButton account;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        start_btn = findViewById(R.id.start_btn);
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SetsCards.class);
+                startActivity(intent);
+            }
+        });
+
+        account = findViewById(R.id.account);
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyProfile.class);
+                startActivity(intent);
+            }
+        });
     }
 }
