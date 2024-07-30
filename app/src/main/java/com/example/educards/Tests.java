766c427174d90e7cc77db716b5b860c89bb279f2
maksym <maksym.varash2007@gmail.com>
2024-07-30 11:45:03 +0300

    have done Tests.java

--- app/src/main/java/com/example/educards/Tests.java
+++ app/src/main/java/com/example/educards/Tests.java
@@ -1,17 +1,64 @@
 package com.example.educards;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Tests extends AppCompatActivity {
 
+    private ImageButton account;
+    private ImageButton messages;
+    private LinearLayout cards;
+    private ConstraintLayout test;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tests);
+
+        account = findViewById(R.id.account);
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Tests.this, MyProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        messages = findViewById(R.id.messages);
+        messages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Tests.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cards = findViewById(R.id.cards);
+        cards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Tests.this, SetsCards.class);
+                startActivity(intent);
+            }
+        });
+
+        test = findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Tests.this, TestTF.class);
+                startActivity(intent);
+            }
+        });
     }
 }
