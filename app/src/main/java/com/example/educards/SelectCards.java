de2d74ea8ce23d25057c487f0f9728e698719610
Mayka <163645840+MaykaXX@users.noreply.github.com>
2024-08-09 17:16:23 +0300

    create ResultCards.java, SelectCards.java

---
+++ app/src/main/java/com/example/educards/SelectCards.java
@@ -0,0 +1,53 @@
+package com.example.educards;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SelectCards extends AppCompatActivity {
+
+    private ImageButton account;
+    private ImageButton messages;
+    private LinearLayout tests;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.selected_cards);
+
+        account = findViewById(R.id.account);
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectCards.this, MyProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        messages = findViewById(R.id.messages);
+        messages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectCards.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        tests = findViewById(R.id.tests);
+        tests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectCards.this, Tests.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
