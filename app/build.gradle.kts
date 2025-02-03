3093ad3a9dbf192fa9fbec76c871a4bdddb0b2fc
mayka <hwangdugon@gmail.com>
2025-02-03 22:31:54 +0200

    try to solve the issue third time: List is empty

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,20 @@
 plugins {
     id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt") // підтримка KAPT
     id("io.freefair.lombok") version "8.0.1"
 }
 
 android {
     namespace = "com.example.educards"
-    compileSdk = 35
+    compileSdk = 34
     buildToolsVersion = "34.0.0"
 
 
     defaultConfig {
         applicationId = "com.example.educards"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -22,7 +24,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
@@ -43,16 +48,17 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.activity:activity:1.8.0")
-    implementation ("androidx.drawerlayout:drawerlayout:1.1.1")
-    implementation ("com.google.android.material:material:1.9.0")
+    implementation("androidx.drawerlayout:drawerlayout:1.1.1")
+    //implementation("com.google.android.material:material:1.9.0")
 
     implementation("redis.clients:jedis:5.2.0")
+    implementation("androidx.mediarouter:mediarouter:1.7.0")
 
     // Room
     val roomVersion = "2.6.1"
 
     implementation("androidx.room:room-runtime:$roomVersion")
-    annotationProcessor("androidx.room:room-compiler:$roomVersion") // Для Kotlin
+    kapt("androidx.room:room-compiler:$roomVersion") // Для Kotlin
     implementation("androidx.room:room-ktx:$roomVersion") // Для інтеграції з Coroutines
 
     // Lombok
@@ -62,4 +68,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
--- app/src/main/java/com/example/educards/Redis.java
+++ app/src/main/java/com/example/educards/Redis.java
@@ -3,13 +3,38 @@ package com.example.educards;
 import redis.clients.jedis.UnifiedJedis;
 
 public class Redis {
-    UnifiedJedis jedis = new UnifiedJedis("redis://localhost:6379");
+    private static Redis instance;
+    private final UnifiedJedis jedis;
 
-    public String setVariable(String Name, String Data) {return jedis.set(Name, Data);}
+    public Redis() {
+        this.jedis = new UnifiedJedis("redis://localhost:6379");
+    }
+    public static synchronized Redis getInstance() {
+        if (instance == null) {
+            instance = new Redis();
+        }
+        return instance;
+    }
+    public String setVariable(String Name, String Data) {
+        return jedis.set(Name, Data);
+    }
 
-    public String getVariable(String Name){return jedis.get(Name);}
+    public String getVariable(String Name) {
+        return jedis.get(Name);
+    }
 
-    public long setHash(String Name, String Object, String Data){return jedis.hset(Name, Object, Data);}
+    public long setHash(String Name, String Object, String Data) {
+        return jedis.hset(Name, Object, Data);
+    }
 
-    public String getHash(String Name, String Object){return jedis.hget(Name, Object);}
+    public String getHash(String Name, String Object) {
+        return jedis.hget(Name, Object);
+    }
+
+// Новый метод для закрытия соединения
+    public void close() {
+        if (jedis != null) {
+            jedis.close();
+        }
+    }
 }
