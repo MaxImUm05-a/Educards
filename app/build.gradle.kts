771b318f9c37e34d1153de16a54006c87ac7eeec
mayka <hwangdugon@gmail.com>
2025-02-26 12:01:53 +0200

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,19 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-//    id("kotlin-kapt") // підтримка KAPT
     id("io.freefair.lombok") version "8.0.1"
     id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.example.educards"
-    compileSdk = 34
-    buildToolsVersion = "34.0.0"
-
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.educards"
         minSdk = 26
-        targetSdk = 34
+        //noinspection EditedTargetSdkVersion
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -46,43 +44,37 @@ android {
 }
 
 dependencies {
-//    implementation("com.google.devtools.ksp:symbol-processing-api:1.9.24-1.0.17")
-//    ksp("com.google.devtools.ksp:symbol-processing:1.9.24-1.0.17")
-
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.activity:activity:1.8.0")
-    implementation("androidx.drawerlayout:drawerlayout:1.1.1")
-    //implementation("com.google.android.material:material:1.9.0")
-
-    implementation("redis.clients:jedis:5.2.0")
-    implementation("androidx.mediarouter:mediarouter:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.activity:activity:1.10.0")
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
+    implementation("androidx.core:core-ktx:1.15.0")
 
     // Room
     val roomVersion = "2.6.1"
-
     implementation("androidx.room:room-runtime:$roomVersion")
-//    annotationProcessor("androidx.room:room-compiler:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
-//    implementation("androidx.room:room-ktx:$roomVersion") // Для інтеграції з Coroutines
+    implementation("androidx.room:room-ktx:$roomVersion")
 
+    // Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.50")
     ksp("com.google.dagger:hilt-compiler:2.50")
 
+    // Redis
+    implementation("redis.clients:jedis:5.2.0")
+    implementation("androidx.mediarouter:mediarouter:1.7.0")
+
     // Lombok
-    compileOnly("org.projectlombok:lombok:1.18.34")
     ksp("org.projectlombok:lombok:1.18.34")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
 
 kotlin {
     jvmToolchain(21)
-}
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "21"
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
     }
 }
